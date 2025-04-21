108aadace680d0ce7c1e79d99a80b44a628a12af
iriska09 <kudaibergenovayryska09@gmail.com>
2025-04-21 07:58:54 -0500

    changed files

--- scripts/amazonlinux_bootstrap.sh
+++ scripts/amazonlinux_bootstrap.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 echo "Updating system..."
-sudo dnf update -y
+sudo dnf upgrade --releasever=2023.7.20250414 -y  # 🚀 Ensures latest packages for AL2023
 
 echo "Installing essential packages..."
-sudo dnf install -y python3 python3-pip python3-libdnf5 ansible amazon-cloudwatch-agent amazon-ssm-agent cronie fail2ban
+sudo dnf install -y python3 python3-pip ansible amazon-cloudwatch-agent amazon-ssm-agent cronie fail2ban
 
 echo "Configuring services..."
 sudo systemctl enable amazon-ssm-agent
